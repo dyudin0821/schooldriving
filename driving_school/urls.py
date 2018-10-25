da12131bd7070d583b39fb2a1ec07e31a70a0538
denistu10 <denistu2191@gmail.com>
2018-10-25 20:20:37 +0300

    add dev branch

--- driving_school/urls.py
+++ driving_school/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('ckeditor/', include('ckeditor_uploader.urls')),
     re_path(r'^', include('schooldriving.urls')),
 ] \
-              # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
-              # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+              + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
